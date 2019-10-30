82bec6ba324757fedc444af2ca704858062674de
rutenis <rutis98@gmail.com>
2019-10-30 16:50:51 +0000

    first commit

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,222 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Homepage_0_document_0_phone.png",
+        "label": "Luno Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Homepage_0_document_1_tablet.png",
+        "label": "Luno Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Homepage_0_document_2_laptop.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Homepage_0_document_2_laptop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Homepage_0_document_2_laptop.png",
+        "label": "Luno Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "laptop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Business_Page_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Business_Page_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Business_Page_0_document_0_phone.png",
+        "label": "Luno Business Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/business",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Business_Page_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Business_Page_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Business_Page_0_document_1_tablet.png",
+        "label": "Luno Business Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/business",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Business_Page_0_document_2_laptop.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Business_Page_0_document_2_laptop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Business_Page_0_document_2_laptop.png",
+        "label": "Luno Business Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/business",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "laptop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Learn_Page_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Learn_Page_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Learn_Page_0_document_0_phone.png",
+        "label": "Luno Learn Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/learn",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Learn_Page_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Learn_Page_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Learn_Page_0_document_1_tablet.png",
+        "label": "Luno Learn Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/learn",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Luno_Learn_Page_0_document_2_laptop.png",
+        "test": "../bitmaps_test/20191030-161623/backstop_default_Luno_Learn_Page_0_document_2_laptop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Luno_Learn_Page_0_document_2_laptop.png",
+        "label": "Luno Learn Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:9000/learn",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "laptop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
